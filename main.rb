5af643bdcda9100f0626e9d03214bc17dd2daa38
afyqeyzar <norafyqe@gmail.com>
2022-08-25 14:00:53 +0100

    method fibo_rec

--- main.rb
+++ main.rb
@@ -22,19 +22,29 @@ end
 
 #p fibs(5)
 
+# def fibs_rec(n)
+#   fibo_seq = [0]
+#   if n == 2
+#     fibo_seq << 1
+#     1
+#   elsif n <= 1
+#     0
+#   else
+#     third = fibs_rec(n-1) + fibs_rec(n-2)
+#     fibo_seq << third
+#     third
+#   end
+#   fibo_seq
+# end
+
 def fibs_rec(n)
-  fibo_seq = [0]
-  if n == 2
-    fibo_seq << 1
-    1
-  elsif n <= 1
-    0
-  else
-    third = fibs_rec(n-1) + fibs_rec(n-2)
-    fibo_seq << third
-    third
-  end
-  fibo_seq
+  # base cases 0 and 1
+  return [0] if n == 0
+  return [0, 1] if n == 1
+
+  array = fibs_rec(n - 1)
+  # add the last two to form new elements
+  array << array[-2] + array[-1]
 end
 
-p fibs_rec(3)
+p fibs_rec(8)
